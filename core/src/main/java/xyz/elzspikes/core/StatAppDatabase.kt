796ee28eba9bb20d2e7cf7ffc2011ea41b315799
Jameido <rossiluca.dev@gmail.com>
2018-09-17 09:43:41 +0200

    Included room library and created database class
    Updated Kotlin version

---
+++ core/src/main/java/xyz/elzspikes/core/StatAppDatabase.kt
@@ -0,0 +1,31 @@
+package xyz.elzspikes.core
+
+import android.arch.persistence.room.Database
+import android.arch.persistence.room.Room
+import android.arch.persistence.room.RoomDatabase
+import android.content.Context
+
+/**
+ * Created by Jameido on 17/09/2018.
+ *
+ * The Room database that contains the schedule and history
+ */
+@Database(entities = arrayOf(), version = 0)
+abstract class StatAppDatabase : RoomDatabase() {
+
+    companion object {
+
+        @Volatile
+        private var sINSTANCE: StatAppDatabase? = null
+
+        fun getInstance(context: Context): StatAppDatabase =
+                sINSTANCE ?: synchronized(this) {
+                    sINSTANCE ?: buildDatabase(context).also { sINSTANCE = it }
+                }
+
+        private fun buildDatabase(context: Context) =
+                Room.databaseBuilder(context.applicationContext,
+                        StatAppDatabase::class.java, "StatApp.db")
+                        .build()
+    }
+}
